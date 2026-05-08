69f649a4e827e0f9b30a01d7be5a61cf56f36598
Joelene241161 <joelenesuenedutoit@gmail.com>
2026-05-08 11:15:15 +0200

    Fixed MERN backend and added seller page
    Installed cors and created a seller page, as well as added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState } from "react";
 import axios from "axios";
 import LogIn from "./Pages/login";
@@ -6,15 +7,17 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/home";
 import Account from "./Pages/account";
 import Product from "./Pages/product";
+import Seller from "./Pages/seller";
 
 function App() {
-
+  
   return (
     <BrowserRouter>
 
       <nav>
         <Link to="/">Home</Link> |
         <Link to="/product">Product</Link> |
+        <Link to="/seller">Seller</Link> |
         <Link to="/login">LogIn</Link> |
         <Link to="/account">Account</Link>
       </nav>
@@ -22,6 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/seller" element={<Seller />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/account" element={<Account />} />
       </Routes>
